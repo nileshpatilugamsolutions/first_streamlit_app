3492287601aebe0cae69f09d3c2402382d5e7a54
nileshpatilugamsolutions <138318280+nileshpatilugamsolutions@users.noreply.github.com>
2023-07-05 08:51:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,18 +37,18 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 ##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_cur.execute("select * from fruit_load_list")
-#my_data_row = my_cur.fetchall()
-#streamlit.header("The fruit load list contains:")
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
 ##streamlit.text("Hello from Snowflake:")
-#streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_row)
 
 #New Section to add fruit
 ##streamlit.header('')
-#new_fruit = streamlit.text_input('What Fruit would you like to add?')
-#streamlit.write('The user entered', new_fruit)
+new_fruit = streamlit.text_input('What Fruit would you like to add?')
+streamlit.write('The user entered', new_fruit)
 
 
